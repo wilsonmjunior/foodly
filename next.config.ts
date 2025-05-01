d5fa944dfdd803595b47a010f3ff600256ee8ff5
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-04-30 23:17:53 -0300

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'unsplash.com',
+                hostname: 'i.pinimg.com',
             },
         ],
     },
